b3684e8cbdeac5bd5fb63414521ed5bbe449c8ec
Withered Flowers <withered-flowers@users.noreply.github.com>
2020-06-26 13:17:08 +0700

    add: start endpoint

---
+++ sources/a-start/app.js
@@ -0,0 +1,11 @@
+/*
+| Endpoint           | Deskripsi                              |
+|:-------------------|:---------------------------------------|
+| GET /              | Menampilkan "hello world"              |
+| GET /ide           | Menampilkan form penambah `Identities` |
+| GET /ide/add       | Menampilkan form penambah `Identities` |
+| POST /ide/add      | Menghandle form penambah `Identities`  |
+| GET /ide/edit/:id  | Menampilkan edit form `Identities`     |
+| POST /ide/edit/:id | Menghandle edit form `Identities`      |
+| GET /ide/del/:id   | Menghandle delete `Identities`         |
+*/
